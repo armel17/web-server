2d958c207523ddc4dd42527689f9a689bbdc800c
Arnaud Melin <arnaudmelin@hotmail.com>
2016-08-26 18:58:31 +0200

    Update language

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(middleware.logger);
 
                   // route-level use of middleware
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('About Us');
+	res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
